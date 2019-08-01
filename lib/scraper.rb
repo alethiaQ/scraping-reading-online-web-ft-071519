a8642c0e9a337448f6616370dc321ee8e927979f
Alethia Quintero <alethiaq22@gmail.com>
2019-08-01 21:31:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".site-header__hero__headline").text
+p doc.css(".site-header__hero__headline").text
