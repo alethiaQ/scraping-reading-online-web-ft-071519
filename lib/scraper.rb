d0dbdc73d204d16286780c005360f6f0303fbf63
Alethia Quintero <alethiaq22@gmail.com>
2019-08-01 21:34:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,6 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 courses = doc.css(".tout__label.heading.heading--level-4")
 
-courses.each do |course| 
-  puts course.text.strip 
+courses.each do |course|
+  puts course.text.strip
 end
